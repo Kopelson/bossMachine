83d4ed64f76f2b03eb29ff5b938cccf8096d9506
Kenneth <kennethkopelson@gmail.com>
2021-06-18 19:52:59 -0700

    added route to get all minions

--- server/minions.js
+++ server/minions.js
@@ -2,3 +2,18 @@ const express = require('express');
 const minionsRouter = express.Router();
 
 module.exports = minionsRouter;
+
+const {getAllFromDatabase} = require('./db');
+
+// GET /api/minions to get an array of all minions.
+minionsRouter.get('/', (req, res, next) => {
+    console.log('You are requesting minions.')
+    const minions = getAllFromDatabase('minions');
+    res.status(200).send(minions)
+})
+
+
+// POST /api/minions to create a new minion and save it to the database.
+// GET /api/minions/:minionId to get a single minion by id.
+// PUT /api/minions/:minionId to update a single minion by id.
+// DELETE /api/minions/:minionId to delete a single minion by id.
