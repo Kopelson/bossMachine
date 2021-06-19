5764ed1289cae079a79ec1d7a18bc39e232088f4
Kenneth <kennethkopelson@gmail.com>
2021-06-18 20:00:47 -0700

    added minion post route

--- server/minions.js
+++ server/minions.js
@@ -3,7 +3,7 @@ const minionsRouter = express.Router();
 
 module.exports = minionsRouter;
 
-const {getAllFromDatabase} = require('./db');
+const {getAllFromDatabase, addToDatabase} = require('./db');
 
 // GET /api/minions to get an array of all minions.
 minionsRouter.get('/', (req, res, next) => {
@@ -14,6 +14,12 @@ minionsRouter.get('/', (req, res, next) => {
 
 
 // POST /api/minions to create a new minion and save it to the database.
+
+minionsRouter.post('/', (req, res, next) => {
+    console.log('You are posting a new minion to the database');
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+})
 // GET /api/minions/:minionId to get a single minion by id.
 // PUT /api/minions/:minionId to update a single minion by id.
 // DELETE /api/minions/:minionId to delete a single minion by id.
